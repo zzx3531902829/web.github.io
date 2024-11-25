7928673f0e5ff01a0a9b7095036a1b23d511660a
zzx3531902829 <3531902829@qq.com>
2024-11-25 21:38:52 +0800

    Add files via upload

---
+++ js/append.js
@@ -0,0 +1,35 @@
+//尊敬的陈老师，js部分的代码借鉴了网上教程，还请谅解
+
+$(()=>{
+	
+	$(".banner").append(`
+	    <div class="nav">
+			<h2>千与千寻</h2>
+			
+	        <div class="as">
+			    <a href="index.html">千寻首页</a>
+			    <a href="01_Juqing.html">剧情简介</a>
+			    <a href="02_Renwu.html">人物介绍</a>
+			    <a href="03_zuozhe.html">作者简介</a>
+			    <a href="https://feng-lan-17.github.io/sushe.github.io/">返回寝室</a>
+			</div>
+		</div>
+	`)
+	
+	$(".footer").append(`
+
+
+        <h2>千与千寻</h2>
+		
+		<div class="copyright">
+		     千与千寻@量子幽灵  所有
+		</div>
+		
+    
+
+    `)
+	
+	
+	
+	
+})
